55ce7b271ff975b2822cb21febc486733d036128
Amisha Bharti <amishab.ug20.ce@nitp.ac.in>
2023-02-08 18:01:42 +0530

    Update Frog_Jump.cpp

--- Frog_Jump.cpp
+++ Frog_Jump.cpp
@@ -15,3 +15,25 @@ int frogJump(int n, vector<int> &heights)
     return dp[n-1];
     
 }
+
+
+///// Frog Jump with K distance
+
+int frogJump(int n,int k, vector<int> &heights)
+{
+    vector<int> dp(n,0);
+    dp[0]=0;
+    for(int i=1;i<n;i++)
+    {
+        int x=INT_MAX;
+        for(int j=1;j<=k;j++)
+        {
+            int y=INT_MAX;
+            if(i-j>=0)y=dp[i-j]+abs(heights[i]-heights[i-j]);
+            x=min(x,y);
+        }
+        dp[i]=x;
+    }
+    return dp[n-1];
+}
+
